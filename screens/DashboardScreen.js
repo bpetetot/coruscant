7366c8697fadc9733b55ab27945d0e31285d9d22
Benjamin Petetot <bpetetot@gmail.com>
2020-01-07 17:42:57 +0100

    :sparkles: add google auth (#1)

---
+++ screens/DashboardScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, Button,  View } from 'react-native';
+
+import firebase from 'firebase'
+
+export default function DashboardScreen({ navigation }) {
+  const signOut = () => {
+    firebase.auth().signOut()
+    navigation.navigate('LoadingScreen')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard screen</Text>
+      <Button title="Signout" onPress={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
